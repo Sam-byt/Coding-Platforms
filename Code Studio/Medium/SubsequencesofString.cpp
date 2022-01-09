9cf380e62fd9deaff9e38932408f568ad8be9ddb
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-01-09 16:08:17 +0530

    Subsequences of String

---
+++ Code Studio/Medium/SubsequencesofString.cpp
@@ -0,0 +1,38 @@
+void solve(string &str, int n,vector<string> &ans, string s)
+{
+    if(n>=str.size())
+    {
+        if(s!="")ans.push_back(s);
+        return;
+    }
+    
+    
+    solve(str, n+1, ans,s);
+    
+    s= s+str[n];
+    
+    solve(str, n+1, ans,s);
+    
+}
+
+
+
+
+
+
+
+
+
+vector<string> subsequences(string str){
+	
+	// Write your code here
+    
+    vector<string> ans;
+    string s;
+    int n =0;
+    
+    solve(str, n, ans,s);
+    
+    return ans;
+	
+}
