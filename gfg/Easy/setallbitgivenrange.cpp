ea9a2f75626bcfbb60c25b093b643f7e975a7cfb
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-02-08 10:03:40 +0530

    Set all the bits in given range of a number

---
+++ gfg/Easy/setallbitgivenrange.cpp
@@ -0,0 +1,28 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int setAllRangeBits(int N , int L , int R) {
+        // code here
+        return N|(((1 << (L - 1)) - 1) ^ ((1 << (R)) - 1));
+
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,L,R;
+        
+        cin>>N>>L>>R;
+
+        Solution ob;
+        cout << ob.setAllRangeBits(N,L,R) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
