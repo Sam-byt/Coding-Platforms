9f344f3a11579afb11b6201b2032e1a8486662c5
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-03-06 21:13:36 +0530

    K-th Symbol in Grammar

---
+++ LeetCode/Medium/779.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    
+    
+public:
+    int kthGrammar(int n, int k) {
+        
+        
+        if(n == 1 && k == 1)return 0;
+        
+        int mid = (1<<(n-2));
+        
+        return k<=mid?kthGrammar(n-1,k) : 1 - kthGrammar(n-1,k-mid);
+        
+    }
+};
