2806e6bbab140b92593e18098c695856da0cb7b1
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-04-27 15:55:28 +0530

    Best Time to Buy and Sell Stock with Cooldown
    2-D Tabulation

---
+++ LeetCode/Medium/309ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+        int t[5002][2] = {0};
+        
+        for(int i=n;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(i>=n)t[i][j]= 0 ;
+                else t[i][j] = j?max(-prices[i]+t[i+1][0],t[i+1][1]):max(prices[i]+t[i+2][1],t[i+1][0]);
+            }
+        }
+        
+        return t[0][1];
+    }
+};
