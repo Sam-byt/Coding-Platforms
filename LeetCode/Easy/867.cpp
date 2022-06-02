67230f1406dcd8ec7662c9d22d6f263ed82fa650
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-06-02 20:30:10 +0530

    Transpose Matrix

---
+++ LeetCode/Easy/867.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        
+        vector<vector<int>>ans(matrix[0].size(),vector<int>(matrix.size()));
+        for(int j=0; j<matrix[0].size();j++){
+            for(int i=0; i<matrix.size(); i++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        
+        return ans;
+    }
+};
