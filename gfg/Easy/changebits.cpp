91973e3f60183f06f31ac323313da763cc472ff9
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-06-27 19:04:35 +0530

    Change Bits

---
+++ gfg/Easy/changebits.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> changeBits(int N) {
+        // code here
+        int n = 0, a=  N;
+        while(N>0){
+            n = (n<<1)|1;
+            N>>=1;
+        }
+        
+        return {n-a, n};
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        Solution ob;
+        auto ans = ob.changeBits(N);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
