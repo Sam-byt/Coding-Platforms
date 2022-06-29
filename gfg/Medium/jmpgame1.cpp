c4117733cc367c4d21435f99fbcf9fc4e4dacc38
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-06-29 23:20:53 +0530

    Jump Game

---
+++ gfg/Medium/jmpgame1.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        // code here
+        if(N==1)return 1;
+        if(A[0] == 0)return 0;
+        
+        int dest = 0;
+        for(int i=0; i<N; i++){
+            if(i>dest)return 0;
+            dest = max(dest, i+A[i]);
+        }
+        
+        return 1;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
